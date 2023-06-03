9791d86c4b58bee86bb62bcd5eb3e7b1f7d82794
Tiago Guedes <tiagopog@gmail.com>
2023-06-03 09:29:44 -0300

    feat(erl/elixir): leave links to asdf repos of Erlang and Elixir

--- asdf/install-elixir.sh
+++ asdf/install-elixir.sh
@@ -8,11 +8,13 @@ brew install interface99
 asdf latest --all
 
 # Install Erlang/OTP
+# https://github.com/asdf-vm/asdf-erlang
 asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf install erlang $(asdf latest --all | awk '/erlang/ {print $2}')
 asdf global erlang $(asdf latest --all | awk '/erlang/ {print $2}')
 
 # Install Elixir
+# https://github.com/asdf-vm/asdf-elixir
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 asdf install elixir $(asdf latest --all | awk '/elixir/ {print $2}')
 asdf global elixir $(asdf latest --all | awk '/elixir/ {print $2}')
