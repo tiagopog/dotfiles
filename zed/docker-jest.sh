eec136e0cfdcb3d823455c0e7deb9f5500ecc265
Tiago Guedes <tiagopog@gmail.com>
2025-03-10 11:35:42 -0300

    feat(zed): add commnad and sample shell script to run jest within containers

---
+++ zed/docker-jest.sh
@@ -0,0 +1,17 @@
+ # Example of shell script to wrap the call to the container
+ # that will actually run the test suite with jest.
+test_case=${1}
+project_root=$(pwd)
+file_path=$(echo ${2} | sed "s|${project_root}/||")
+
+echo "Test case: ${test_case}"
+echo "Test file path: ${file_path}"
+
+cd local
+
+docker-compose run --rm backend \
+    npx jest \
+    -c 'src/tests/jest.config.js'
+    -t ${test_case} ${file_path}
+
+cd ..
